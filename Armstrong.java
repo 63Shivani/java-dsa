b994ac648fdb986c26407e4cd7b3207b89e281d9
Shivani Kumari <100162080+63Shivani@users.noreply.github.com>
2023-06-20 11:45:34 +0530

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,26 @@
+package bank;
+
+
+import java.util.Scanner;
+
+public class isArmstrong {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(Armstrong(n));
+    }
+
+    static boolean Armstrong(int n) {
+        int original = n;
+        int sum = 0;
+        while (n > 0) {
+            int rem = n % 10;
+            n /= 10;
+            sum = sum + rem * rem * rem;
+        }
+        if(sum==original){
+            return true;
+        }
+        return false;
+    }
+}
